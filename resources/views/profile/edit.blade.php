18b80f699530b4eba46de4d84114d46a4b6d9fa2
aldy57 <aldyalfiansyah06@gmail.com>
2026-07-25 21:06:35 +0700

    fix layout profil dashboard

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.app')
 
 @section('content')
-<div x-data="{ showQrModal: false }" class="max-w-4xl mx-auto space-y-6">
+<div x-data="{ showQrModal: false }" class="max-w-7xl mx-auto space-y-6">
     <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-6 gap-4">
         <div>
             <h1 class="text-3xl font-bold">Profil Akun</h1>
